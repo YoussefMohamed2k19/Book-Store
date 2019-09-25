2a8488f3cf68b651a02331dbc53e46b0fccc0401
unknown <elpopyoussef@gmail.com>
2019-09-25 20:48:39 +0200

    Added new line files

--- import.py
+++ import.py
@@ -4,7 +4,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine("postgres://cizlmooelkynwb:2423acbbe51bf4908f28f10c6edc3adddbf2f7cf8aae2be53be57edd0684b010@ec2-174-129-27-158.compute-1.amazonaws.com:5432/dfo8gj0a4g5mo0")
+engine = create_engine("postgres://veufgqrqcdagor:7705973df1ee8412f8adf05f713f3a4c2692d3f79fe7df60101315add082c144@ec2-54-225-95-183.compute-1.amazonaws.com:5432/d6ap38ig08mt8t")
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
